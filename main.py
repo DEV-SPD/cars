4605164fc1a4e9c205a464e0b1e6410194ffc9b9
Siba Prasad Das <103878703+DEV-SPD@users.noreply.github.com>
2023-08-07 16:09:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def predict_price():
     df2 = preprocessor(df)
 
     result = model.predict(df2)
-    return jsonify{'price' : 'result'}
+    return str(result)
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
